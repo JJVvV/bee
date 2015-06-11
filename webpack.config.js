64bc4f6850daef3d39c5f78c7718fb5250f8e7e0
JJVvV <drowning524@gmail.com>
2015-06-11 19:52:44 +0800

    bee

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://192.168.0.114:3000',
+    'webpack/hot/only-dev-server',
+    './src/App'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css|\.scss/,
+        loaders:['style', 'css', 'sass']
+      }
+    ]
+  },
+    debug:true
+};
